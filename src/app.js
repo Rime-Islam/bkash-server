b2bed28ade4cad372c1520a18efd65ce08adae24
rime-islam <rimeislam672@gmail.com>
2025-02-25 20:34:25 +0600

    first commit

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+import config from "./app/config/index.js";
+
+const app = express();
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+const startServer = async () => {
+  try {
+    await mongoose.connect(config.db_url);
+    console.log("Connected to the database");
+
+    app.listen(5000, () => {
+      console.log(`Server is running on port 5000`);
+    });
+  } catch (error) {
+    console.error("Database connection failed:", error);
+  }
+};
+
+startServer();
+
+export default app;
---
+++ src/app/config/index.js
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+
+export default {
+    port: process.env.PORT,
+    db_url: process.env.DB_URL,
+    bcrypt_salt_round: process.env.BCRYPT_SALT_ROUND,
+    jwt_access_token: process.env.ACCESS_TOKEN_SECRET,
+    URL: process.env.FRONTEND_URL,
+    user_name: process.env.EMAIL_USER,
+    password: process.env.EMAIL_PASS,
+};
